beccefe703d3c9caa72a12321a9aca919cf5a140
philiptang42 <philiptang42@gmail.com>
2015-11-19 19:42:08 -0500

    added souq array

--- code.rb
+++ code.rb
@@ -1,3 +1,10 @@
 puts "Hi. What's your name?"
 name = gets.chomp
 puts "Hello #{name}."
+
+souq_array = ["old paperback book",
+"potato", "red onion", "dried lemon",
+"frankincense", "medicinal herbs", "saffron",
+"glass spice jar", "red fabric", "orange fabric",
+"handicraft", "small Persian rug", "medium Persian rug",
+"large Persian rug", "extra large Persian rug"]
