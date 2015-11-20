31ece82cf1aa9a60a43c6769832821e241a5d18b
philiptang42 <philiptang42@gmail.com>
2015-11-20 18:05:53 -0500

    step 1 complete

--- code.rb
+++ code.rb
@@ -1,4 +1,6 @@
-souq = []
+custom_souq = []
+
+selection = ""
 
 souq_items = ["old paperback book",
 "potato", "red onion", "dried lemon",
@@ -10,3 +12,23 @@ souq_items = ["old paperback book",
 puts "Hi. What's your name?"
 name = gets.chomp
 puts "Hello #{name}."
+
+while true
+  puts "Here's what we have in stock:"
+  souq_items.each do |item|
+    puts "* #{item}"
+  end
+  puts "What are you going to buy? (enter 'finished' when done shopping)"
+  selection = gets.chomp.downcase
+  if selection == "finished"
+    break
+  elsif souq_items.include?(selection)
+    custom_souq << selection
+    puts selection.to_s + " has been added!"
+  else
+    puts "We don't have those in stock. Pick something we do."
+  end
+end
+  puts "#{name}, thanks for shopping. Here's your total selection:"
+  custom_souq.each do |item| puts "* #{item}"
+end
