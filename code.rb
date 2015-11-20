0f999b0c680c63ab32b7d0000a2b9a515a9c8002
philiptang42 <philiptang42@gmail.com>
2015-11-19 19:45:07 -0500

    souq

--- code.rb
+++ code.rb
@@ -1,10 +1,12 @@
-puts "Hi. What's your name?"
-name = gets.chomp
-puts "Hello #{name}."
+souq = []
 
-souq_array = ["old paperback book",
+souq_items = ["old paperback book",
 "potato", "red onion", "dried lemon",
 "frankincense", "medicinal herbs", "saffron",
 "glass spice jar", "red fabric", "orange fabric",
 "handicraft", "small Persian rug", "medium Persian rug",
 "large Persian rug", "extra large Persian rug"]
+
+puts "Hi. What's your name?"
+name = gets.chomp
+puts "Hello #{name}."
